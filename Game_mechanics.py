a5c2307b8ae5d850c45588143cb014b969a7cb42
xpple-o-O <gavinchen2020@gmail.com>
2023-03-25 15:25:27 -0400

    upddate

--- Game_mechanics.py
+++ Game_mechanics.py
@@ -3,16 +3,35 @@ import blocks
 
 
 def rotate_clockwise(shape):
-    rows = len(shape) -1
-    columns = len(shape[0]) -1
-    for item_row in shape:
+    rows = len(shape)
+    columns = len(shape[0])
+    rotated_item = []
+    new_list = []
+    for i in range(columns):
+        for index in range(len(shape)):
+            new_list.append(shape[index][i])
+        rotated_item.append(new_list)
+        new_list = []
+    return rotated_item
 
+
+def rotate_counter_clockwise(shape):
+    rows = len(shape)
+    columns = len(shape[0])
+    rotated_item = []
+    new_list = []
+    for i in range(columns - 1, 0, -1):
+        for index in range(len(shape)- 1, 0, -1):
+            new_list.append(shape[index][i])
+        rotated_item.append(new_list)
+        new_list = []
+    return rotated_item
 
 
 # def place_block(shape):
 
 
-def spawn_item(item):
-    if item == blocks.I:
-        spawn("row 22", "column 5,6")
-rotate_clockwise()
+# def spawn_item(item):
+#     if item == blocks.I:
+#         spawn("row 22", "column 5,6")
+print(rotate_counter_clockwise(blocks.S))
