c84124b288608add252b4d236fd16a070b5f5972
xpple-o-O <gavinchen2020@gmail.com>
2023-03-25 14:33:18 -0400

    upddate

--- Game_mechanics.py
+++ Game_mechanics.py
@@ -2,6 +2,12 @@
 import blocks
 
 
+def rotate_clockwise(shape):
+    return [[shape[y][x]
+             for y in range(len(shape))]
+            for x in range(len(shape[0]) - 1, -1, -1)]
+
+
 def spawn_item(item):
     if item == blocks.I:
         spawn("row 22", "column 5,6")
