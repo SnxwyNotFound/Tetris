e83d86025e7223739abd61719cdcc65880165655
xpple-o-O <gavinchen2020@gmail.com>
2023-03-25 14:49:51 -0400

    upddate

--- Game_mechanics.py
+++ Game_mechanics.py
@@ -6,6 +6,7 @@ def rotate_clockwise(shape):
     return [[shape[y][x]
              for y in range(len(shape))]
             for x in range(len(shape[0]) - 1, -1, -1)]
+def place_block(shape)
 
 
 def spawn_item(item):
