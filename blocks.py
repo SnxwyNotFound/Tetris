84b599746061ba00e7274cb483ff66a67967890a
aarabhib <aarabhibala@gmail.com>
2023-04-15 14:48:41 -0400

    changed shapes to list_shapes

--- blocks.py
+++ blocks.py
@@ -39,12 +39,19 @@ list_shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
 
 class Shape():
-    def __init__(self, shapes):
-        self.shapes = shapes
-        self.color = shape_colors[shapes.index(shapes)]
+    def __init__(self, list_shapes):
+        self.list_shapes = list_shapes
+        self.color = shape_colors[list_shapes.index(list_shapes)]
         self.rotation = 0  # number from 0-3
 
 def get_shape():
-    global shapes, shape_colors
+    global list_shapes, shape_colors
 
-    return Shape(random.choice(shapes))
+    return Shape(random.choice(list_shapes))
+
+##### roations in game mechanics
+def rotate_shape(shape, rotation):
+    new_shape = []
+    #Somehow have to make the shapes rotate and return
+    new_shape.append(shape[rotation])
+    new_shape
